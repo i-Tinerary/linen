e9ad10e1408797852c224c3d7d0925f13e904a98
John Children <john@infosum.com>
2017-11-25 14:38:00 +0000

    Adds basic preferences layout

--- Main.elm
+++ Main.elm
@@ -1,24 +1,34 @@
 module Main exposing (..)
 
+import List
 import Html exposing (..)
 import Html.Attributes exposing (href, class, style)
 import Material
 import Material.Color as Color
+import Material.Chip as Chip
 import Material.Scheme
 import Material.Layout as Layout
+import Material.Table as Table
+import Material.Grid exposing (grid, cell, size, Device(..))
+
+type alias Interest = String
+type alias CategoryName = String
+
+type Category = Category CategoryName (List Interest)
+
+type alias Tab =
+    Int
 
 
-type alias Tab
-    = Int
 
 -- MODEL
 
-
 type alias Model =
-    { mdl:
+    { mdl :
         Material.Model
         -- Boilerplate: model store for any and all Mdl components you use.
     , selectedTab : Tab
+    , selectedCategories : List Category
     }
 
 
@@ -28,6 +38,7 @@ model =
         Material.model
         -- Boilerplate: Always use this initial Mdl model store.
     , selectedTab = 0
+    , selectedCategories = [Category "foo" ["hehe"], Category "bar" [ "bing", "bang", "bong" ]]
     }
 
 
@@ -37,6 +48,7 @@ model =
 
 type Msg
     = SelectTab Tab
+    | AddPreference String
     | Mdl (Material.Msg Msg)
 
 
@@ -47,6 +59,9 @@ update msg model =
             ( { model | selectedTab = tab }
             , Cmd.none
             )
+
+        AddPreference pref ->
+            ( model, Cmd.none )
 
         -- Boilerplate: Mdl action handler.
         Mdl msg_ ->
@@ -73,8 +88,45 @@ view model =
             { header = [ h1 [ style [ ( "padding", "2rem" ) ] ] [ text "I-tinerary" ] ]
             , drawer = []
             , tabs = ( [ text "Preferences", text "Plan" ], [] )
-            , main = []
+            , main = [ pageLayout (mainBody model) ]
             }
+
+
+pageLayout : Html Msg -> Html Msg
+pageLayout html =
+    grid []
+        [ cell [ size All 4 ]
+            [ html ]
+        ]
+
+
+mainBody : Model -> Html Msg
+mainBody model =
+    case model.selectedTab of
+        0 ->
+            preferencesView model
+
+        _ ->
+            text "error"
+
+
+preferencesView : Model -> Html Msg
+preferencesView model =
+    let 
+        preference : String -> Html Msg
+        preference entry =
+            Chip.span [] [ Chip.content [] [text entry] ]
+
+        row : Category -> Html Msg
+        row (Category name entries) =
+        Table.tr []
+            [ Table.td [] [ text name ]
+            , Table.td [] (List.map preference entries)
+            ]
+    in
+       Table.table []
+            [Table.thead [] [], Table.tbody [] (List.map row model.selectedCategories)]
+
 
 main : Program Never Model Msg
 main =
